1bed900471330a4e4cba603bbaa17521c2c9a7e9
Admin <Admin@DESKTOP-POQE336>
2019-08-07 13:48:21 +1200

    chnages updates

---
+++ UnitTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTesting")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("912358bb-9c69-4af2-9fd0-f8f7d4e0a9a5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTesting/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using rr_game;// calling the main program
+
+namespace UnitTesting
+{
+
+    [TestClass]
+    public class UnitTest1
+    //defining and object in class "game"
+    {
+        game testing = new game();
+
+        [TestMethod]
+        public void TestMethod1()
+
+        {
+            //act- need to provide some data to get and output
+            var act = testing.chngebulet(4);
+            //assert - need to check the output with the expected data is wheather correct or not
+            Assert.AreEqual(expected: 5, act);
+        }
+
+        [TestMethod]
+        public void testmethod2()
+        {
+            //act- need to provide some data to get and output
+            var act = testing.chngebulet(4);
+            //assert - need to check the output with the expected data is wheather correct or not
+            Assert.AreNotEqual(notExpected: 4, act);
+
+        }
+    }
+}
+
